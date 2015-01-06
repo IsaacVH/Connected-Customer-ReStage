db33bed2ef6e16b69b5202005eb9e62ebcacc2e7
IsaacVH <isaac.vanh@gmail.com>
2015-01-06 12:27:55 -0500

    Added lots of new stuff.

--- src/php-modules/pieces/carousel.php
+++ src/php-modules/pieces/carousel.php
@@ -1,4 +1,3 @@
-<link href="../css/carousel.css" rel="stylesheet">
 <script>
 	$(function() {			
 		$(".carousel").swipe({
@@ -40,49 +39,16 @@
 	document.addEventListener('DOMContentLoaded', loaded, false);
 </script>
 <div class="carousel">
-<div class="background">
-	<div id="nav">
-		<!-- <div id="prev" onclick="myScroll.scrollToPage('prev', 0);return false">&larr; prev</div> -->
-		<ul id="indicator">
-		</ul>
-		<!-- <div id="next" onclick="myScroll.scrollToPage('next', 0);return false">next &rarr;</div> -->
-	</div>
+	<div class="background">
+		<div id="nav">
+			<ul id="indicator"></ul>
+		</div>
 		<div id="wrapper">
 			<div id="scroller">
 				<ul class="container-list">
-					<?php require("carousel-pages/main-pages.php"); ?>
+					<?php require($carouselpage); ?>
 				</ul>
 			</div>
 		</div>
-</div>
-	<!--
-	<div class="navigation-bar">
-		<div class="nav-container">
-			<div class="nav-circle" name="page5"></div>
-			<div class="nav-circle" name="page4"></div>
-			<div class="nav-circle" name="page3"></div>
-			<div class="nav-circle" name="page2"></div>
-			<div class="nav-circle highlight" name="page1"></div>
-		</div>
 	</div>
-	<div class="container-list">
-		<br>
-		<div class="container" name="page5"><?php 
-			// $carouselpage = "../json/main-page/menu_item_page5.json";
-			// include("six-block-menu.php"); ?></div>
-		<div class="container" name="page4"><?php 
-			// $carouselpage = "../json/main-page/menu_item_page4.json";
-			// include("six-block-menu.php"); ?></div>
-		<div class="container" name="page3"><?php
-			// $carouselpage = "../json/main-page/menu_item_page3.json";
-			// include("six-block-menu.php"); ?></div>
-		<div class="container" name="page2"><?php 
-			// $carouselpage = "../json/main-page/menu_item_page2.json";
-			// include("six-block-menu.php"); ?></div>
-		<div class="container" name="page1"><?php 
-			// $carouselpage = "../json/main-page/menu_item_page1.json";
-			// include("six-block-menu.php"); ?></div>
-	</div>
-	-->
-	<!-- <div id="swipe-hand"> <img src="../assets/Swipe-Hand.png"></img> </div> -->
 </div>
